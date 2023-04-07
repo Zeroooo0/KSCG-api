4ddd40d66345ac35e0a0473087b0a0d212f71e69
Zeroooo0 <markocrncevic.92@gmail.com>
2023-04-07 15:13:39 +0200

    Competitors Public Fix

--- routes/api.php
+++ routes/api.php
@@ -26,9 +26,9 @@ Route::group(['prefix' => 'v1/public'], function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
     Route::get('/clubs', [ClubsController::class, 'public']);  
-    Route::get('/clubs/{club}', [ClubsController::class, 'show_public']);  
+    Route::get('/club/{club}', [ClubsController::class, 'show_public']);  
     Route::get('/competitors', [CompatitorsController::class, 'public']);
-    Route::get('/competitors/{competitor}', [CompatitorsController::class, 'show_public']);
+    Route::get('/competitor/{competitor}', [CompatitorsController::class, 'show_public']);
     Route::get('/competitions', [CompatitionsController::class, 'public']);
     Route::get('/competition-categories/{competition}', [CompatitionsController::class, 'piblicCategories']);
     Route::get('/competition-results/{competition}', [CompatitionsController::class, 'piblicRegistrations']);
