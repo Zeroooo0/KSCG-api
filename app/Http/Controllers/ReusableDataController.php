8d66c81405a0c18aaeff2dab1c00a113f01a17f9
Zeroooo0 <markocrncevic.92@gmail.com>
2023-04-20 00:58:35 +0200

    Finish

--- app/Http/Controllers/ReusableDataController.php
+++ app/Http/Controllers/ReusableDataController.php
@@ -154,7 +154,7 @@ class ReusableDataController extends Controller
     public function clubsResults(Request $request, Club $club)
     {
         $perPage = $request->per_page;
-        return ResultsResource::collection($club->registrations);
+        return ResultsResource::collection(Registration::where('club_id', $club->id)->paginate($perPage));
     }
     public function registeredClubs(Request $request) 
     {
