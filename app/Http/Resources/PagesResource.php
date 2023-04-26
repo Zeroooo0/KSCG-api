ff8cac085415ca04aebf703cf572454124d2519f
Zeroooo0 <markocrncevic.92@gmail.com>
2023-04-26 17:00:31 +0200

    Filter i male izmjene

--- app/Http/Resources/PagesResource.php
+++ app/Http/Resources/PagesResource.php
@@ -16,7 +16,7 @@ class PagesResource extends JsonResource
     {
         date_default_timezone_set('Europe/Amsterdam');
         $userData = $this->user_id;
-        if($this->user_id != null) {
+        if($this->user != null) {
             $userData = [
                 'id' => (string)$this->user->id,
                 'name' => $this->user->name,
