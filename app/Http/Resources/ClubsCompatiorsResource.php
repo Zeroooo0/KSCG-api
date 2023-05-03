025352c7bb1c54400a8b63d5b4f1886895dcbd47
Zeroooo0 <markocrncevic.92@gmail.com>
2023-05-03 14:06:37 +0200

    Club choach lsit added

--- app/Http/Resources/ClubsCompatiorsResource.php
+++ app/Http/Resources/ClubsCompatiorsResource.php
@@ -14,12 +14,23 @@ class ClubsCompatiorsResource extends JsonResource
      */
     public function toArray($request)
     {
+        $storage_url = env('APP_URL') . 'api/file/';
+        if($this->image != null) {
+            $path =  $storage_url . $this->image->url;
+        } else {
+            if($this->gender == 1) {
+                $path = $storage_url . 'default/default-m-user.jpg';
+            } else{
+                $path = $storage_url . 'default/default-f-user.jpg';
+            }
+        }
         return [
             'id' => (string)$this->id,
             'name' => $this->name . ' ' . $this->last_name,   
             'kscgId' => $this->kscg_compatitor_id,
             'brthDay' => date($this->date_of_birth),      
-            'belt' => $this->belt
+            'belt' => $this->belt,
+            'image' => $path
         ];
     }
 }
