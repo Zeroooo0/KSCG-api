61ab07f57d63dd1b8331e4983d30021276a3962e
Zeroooo0 <markocrncevic.92@gmail.com>
2023-05-31 22:29:18 +0200

    add total groups on tatami

--- app/Http/Resources/TimeTableResource.php
+++ app/Http/Resources/TimeTableResource.php
@@ -3,12 +3,15 @@
 namespace App\Http\Resources;
 
 use App\Models\Category;
+use App\Models\Compatition;
 use App\Models\Pool;
 use App\Models\PoolTeam;
+use App\Traits\LenghtOfCategory;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TimeTableResource extends JsonResource
 {
+    use LenghtOfCategory;
   
     /**
      * Transform the resource into an array.
@@ -19,13 +22,14 @@ class TimeTableResource extends JsonResource
     public function toArray($request)
     {
         $category = Category::where('id', $this->category_id)->first();
+        $competiton = Compatition::where('id', $this->compatition_id)->first();
         $kata_or_kumite = $category->kata_or_kumite ? 'Kate' : 'Kumite';
         $gender = $category->gender == 1 ? 'M' : ($category->gender == 2 ? 'Ž' : 'M + Ž');
         $ekipno = $category->solo_or_team ? null : ' | Ekipno';
         $pool = $category->solo_or_team == 1 ? Pool::where('compatition_id', $this->compatition_id)->where('category_id', $this->category_id)->get() : PoolTeam::where('compatition_id', $this->compatition_id)->where('category_id', $this->category_id)->get();
         $pools = PoolResource::collection($pool);
         $poolsTeam = PoolsTeamResource::collection($pool);
- 
+        $groupsTotal = $this->categoryDuration($competiton, $category);
         
         $data = $ekipno == null  ? $pools->whereIn('pool_type', ['G', 'SF', 'FM']) : $poolsTeam->whereIn('pool_type', ['G', 'SF', 'FM']);
         $repesaz = $ekipno == null  ? $pools->whereIn('pool_type', ['R', 'RSF', 'RFM']) : $poolsTeam->whereIn('pool_type', ['R', 'RSF', 'RFM']);
@@ -59,6 +63,7 @@ class TimeTableResource extends JsonResource
             'startedAt' => $this->started_time != null ? date('H:i', strtotime($this->started_time)) : null,
             'finishedAt' => $this->finish_time != null ? date('H:i', strtotime($this->finish_time)) : null,
             'status' => $this->status,
+            'groupsTotal' => $groupsTotal['categoryPoolsFront'],
             'groups' => $request->has('embed') && str_contains($request->embed, 'groups') ? $data->toArray() : 'embbedable',
             'rematch' => $request->has('embed') && str_contains($request->embed, 'rematch') ? $repesaz->toArray() : 'embbedable'
 
