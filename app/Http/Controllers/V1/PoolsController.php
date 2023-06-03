935eb9405553b740c0604f4e03b89a0559741ed9
Zeroooo0 <markocrncevic.92@gmail.com>
2023-06-03 03:31:00 +0200

    testing

--- app/Http/Controllers/V1/PoolsController.php
+++ app/Http/Controllers/V1/PoolsController.php
@@ -96,30 +96,46 @@ class PoolsController extends Controller
             
             
             $timeTracking = $category_timeStart;
+            $registrations = $competition->registrations->where('category_id', $category->id);
+            $timePerCategory = $category->match_lenght;
+            $teamOrSingle = $category->solo_or_team;
+            $teamRegistration = $registrations->groupBy('team_id')->count();
+        
+            $registrationCount = $teamOrSingle == 1 ? $registrations->count() : $teamRegistration;
+        
+            $repesaz = $category->repesaz == true ? $timePerCategory : 0;
+            $totalTimePerCat = 0;
+            $groups = 0;
+            $pools = 0;
             
             switch($registrationCount) {
                 case $registrationCount <= 2:
                     $totalTimePerCat = $registrationCount / 2 * $timePerCategory;
+                    $neededReg = 1;
                     $groups = 0;
                     $pools = 0;
                     break;
                 case $registrationCount <= 4:
                     $totalTimePerCat = $registrationCount / 2 * $timePerCategory + $timePerCategory + $repesaz;
+                    $neededReg = 3;
                     $groups = 1;
                     $pools = 1;
                     break;
                 case $registrationCount <= 8:
                     $totalTimePerCat = $registrationCount / 2 * $timePerCategory + 2 * $timePerCategory + $timePerCategory + 2 * $repesaz;
+                    $neededReg = 7;
                     $groups = 3;
                     $pools = 2;
                     break;
                 case $registrationCount <= 16:
                     $totalTimePerCat = $registrationCount / 2 * $timePerCategory + 4 * $timePerCategory + 2 * $timePerCategory + $timePerCategory + 3 * $repesaz;
+                    $neededReg = 15;
                     $groups = 7;
                     $pools = 3;
                     break;
                 case $registrationCount <= 32:
                     $totalTimePerCat = $registrationCount / 2 * $timePerCategory + 8 * $timePerCategory + 4 * $timePerCategory + 2 * $timePerCategory + $timePerCategory + 4 * $repesaz;
+                    $neededReg = 31;
                     $groups = 15;
                     $pools = 4;
                     break;
@@ -170,7 +186,7 @@ class PoolsController extends Controller
                 for($i = 0; $i <= $counting; $i++) {
                     
                     $first = $i;
-                    $second = ($counting * 2 + 1) - $i;
+                    $second = $neededReg - $i;
                     $inputTeam['compatition_id'] = $compatition->id;
                     $inputTeam['category_id'] = $category_id;
                     $inputTeam['pool'] = $j;
