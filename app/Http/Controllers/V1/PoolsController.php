16f1f8ed84fe858ff52c1710c1a2158be7a40769
Zeroooo0 <markocrncevic.92@gmail.com>
2023-06-03 02:48:59 +0200

    testing

--- app/Http/Controllers/V1/PoolsController.php
+++ app/Http/Controllers/V1/PoolsController.php
@@ -136,7 +136,7 @@ class PoolsController extends Controller
                         $groupType = 'FM';
                         break;
                 }                
-                return Arr::get($val, '3.0.team_id');
+                return respons()->json(Arr::get($val, '3.0.team_id'));
                 for($i = 0; $i <= $counting; $i++) {
                     $random = rand(0,1);
                     $first = $random  ? $i : $counting - $i;
