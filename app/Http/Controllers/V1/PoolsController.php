0e91b39dee7a8b63ccc3e9b0813a6eac87b2c215
Zeroooo0 <markocrncevic.92@gmail.com>
2023-06-03 03:48:35 +0200

    testing

--- app/Http/Controllers/V1/PoolsController.php
+++ app/Http/Controllers/V1/PoolsController.php
@@ -114,6 +114,7 @@ class PoolsController extends Controller
                     $groups = 0;
                     $groupsReal = 1;
                     $pools = 0;
+                    $pool = 1;
                     break;
                 case $registrationCount <= 4:
                     $totalTimePerCat = $registrationCount / 2 * $timePerCategory + $timePerCategory + $repesaz;
@@ -121,6 +122,7 @@ class PoolsController extends Controller
                     $groups = 1;
                     $groupsReal = 2;
                     $pools = 1;
+                    $pool = 2;
                     break;
                 case $registrationCount <= 8:
                     $totalTimePerCat = $registrationCount / 2 * $timePerCategory + 2 * $timePerCategory + $timePerCategory + 2 * $repesaz;
@@ -128,6 +130,7 @@ class PoolsController extends Controller
                     $groups = 3;
                     $groupsReal = 4;
                     $pools = 2;
+                    $pool = 3;
                     break;
                 case $registrationCount <= 16:
                     $totalTimePerCat = $registrationCount / 2 * $timePerCategory + 4 * $timePerCategory + 2 * $timePerCategory + $timePerCategory + 3 * $repesaz;
@@ -135,6 +138,7 @@ class PoolsController extends Controller
                     $groups = 7;
                     $groupsReal = 8;
                     $pools = 3;
+                    $pool = 4;
                     break;
                 case $registrationCount <= 32:
                     $totalTimePerCat = $registrationCount / 2 * $timePerCategory + 8 * $timePerCategory + 4 * $timePerCategory + 2 * $timePerCategory + $timePerCategory + 4 * $repesaz;
@@ -142,16 +146,18 @@ class PoolsController extends Controller
                     $groups = 15;
                     $groupsReal = 16;
                     $pools = 4;
+                    $pools = 5;
                     break;
                 case $registrationCount <= 64:
                     $totalTimePerCat = $registrationCount / 2 * $timePerCategory + 16 * $timePerCategory + 8 * $timePerCategory + 4 * $timePerCategory + 2 * $timePerCategory + $timePerCategory + 5 * $repesaz;
                     $groups = 31;
                     $groupsReal = 32;
                     $pools = 5;
+                    $pools = 6;
                     break;
             }
-            $count = $groupsReal;
-            for($j = 1; $j <= $pools; $j++) {
+            $count = $groups;
+            for($j = 1; $j <= $pool; $j++) {
                 $counting = $count;
                 switch($j) {
                     case $j == 1:
