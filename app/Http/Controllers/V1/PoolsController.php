cc48966ec813b3c4cc7b9a5d6b08c888c4f6f270
Zeroooo0 <markocrncevic.92@gmail.com>
2023-06-03 02:18:26 +0200

    testing

--- app/Http/Controllers/V1/PoolsController.php
+++ app/Http/Controllers/V1/PoolsController.php
@@ -137,10 +137,10 @@ class PoolsController extends Controller
                         break;
                 }                
                 
-                for($i = 0; $i <= ($counting - 1); $i++) {
+                for($i = 0; $i <= ($count - 1); $i++) {
                     $random = rand(0,1);
-                    $first = $random  ? $i : ($counting / 2 ) + $i;
-                    $second = $random ? ($counting / 2 ) + $i : $i;
+                    $first = $random  ? $i : ($count - 1) + $i;
+                    $second = $random ? ($count - 1 ) + $i : $i;
                     $inputTeam['compatition_id'] = $compatition->id;
                     $inputTeam['category_id'] = $category->id;
                     $inputTeam['pool'] = $j;
