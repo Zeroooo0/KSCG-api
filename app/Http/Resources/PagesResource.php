7e24607beb36a8fb230121dd62f1a5e53495a1ee
Zeroooo0 <markocrncevic.92@gmail.com>
2023-06-08 19:34:57 +0200

    Change for components

--- app/Http/Resources/PagesResource.php
+++ app/Http/Resources/PagesResource.php
@@ -90,7 +90,7 @@ class PagesResource extends JsonResource
             'user' => $userData,
             'coverImage' => $cover_image,
             'images' => $image,
-            'components' => $componentsArray != [] ? $componentsArray :  ComponentResource::collection($components),
+            'components' => $componentsArray != [] ? $componentsArray :  ComponentResource::collection($components->sortBy('id', 'desc')),
 
         ];
     }
