e33384d1f129bd850e8114428eee03b479d43a78
Zeroooo0 <markocrncevic.92@gmail.com>
2023-06-08 19:38:08 +0200

    Change for components

--- app/Http/Resources/ComponentResource.php
+++ app/Http/Resources/ComponentResource.php
@@ -19,9 +19,9 @@ class ComponentResource extends JsonResource
             'title' => $this->title,
             'type' => $this->type,
             'orderNo' => $this->order_number,
-            'documents' => $request->has('embed') && str_contains($request->embed, 'documents') ? DocumentsResource::collection($this->documents) : 'embeddable',
-            'images' => $request->has('embed') && str_contains($request->embed, 'images') ? ImageResource::collection($this->images) : 'embeddable',
-            'roles' => $request->has('embed') && str_contains($request->embed, 'roles') ? RolesResource::collection($this->roles) : 'embeddable'
+            'documents' => $request->has('embed') && str_contains($request->embed, 'documents') ? DocumentsResource::collection($this->documents->orderBy('id', 'desc')) : 'embeddable',
+            'images' => $request->has('embed') && str_contains($request->embed, 'images') ? ImageResource::collection($this->images->orderBy('id', 'desc')) : 'embeddable',
+            'roles' => $request->has('embed') && str_contains($request->embed, 'roles') ? RolesResource::collection($this->roles->orderBy('id', 'desc')) : 'embeddable'
         ];
     }
 }
