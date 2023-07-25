969abeb0303223a49a565194ce240c9188343b3f
Zeroooo0 <markocrncevic.92@gmail.com>
2023-07-25 22:18:45 +0200

    Added seminars and many more things

--- routes/api.php
+++ routes/api.php
@@ -197,7 +197,7 @@ Route::group(['prefix' => 'v1', 'middleware' => ['auth:sanctum', 'ability:admin,
     Route::get('/seminars-form-application/{seminar}', [SeminarApplicationController::class, 'index']);
     Route::get('/seminars-morph-application/{seminar}', [SeminarApplicationController::class, 'indexMorph']);
     //
-    Route::get('/compatition-results-calculate/{compatition}', [RegistrationsController::class, 'calculateResultsNow']);
+    Route::post('/compatition-results-calculate/{compatition}', [RegistrationsController::class, 'calculateResultsNow']);
 });
 //testing
 
