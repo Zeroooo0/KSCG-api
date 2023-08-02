d282a29d82da49fe02844f8698e8360af70274f5
Zeroooo0 <markocrncevic.92@gmail.com>
2023-08-02 11:23:23 +0200

    fix sort of registrations

--- app/Http/Controllers/V1/RegistrationsController.php
+++ app/Http/Controllers/V1/RegistrationsController.php
@@ -40,7 +40,11 @@ class RegistrationsController extends Controller
             }
             if(Auth::user()->user_type == 0) {
                 $clubId = Auth::user()->club->id;
-                return RegistrationsResource::collection(Registration::orderBy($sort, $sortDirection)->where('compatition_id', $competitionId)->where('club_id', $clubId)->paginate($per_page));
+                if($competition->is_abroad != 1) {
+                    return RegistrationsResource::collection(Registration::orderBy($sort, $sortDirection)->where('compatition_id', $competitionId)->where('club_id', $clubId)->paginate($per_page));
+                } else {
+                    return RegistrationsResource::collection(Registration::orderBy('id', 'desc')->where('compatition_id', $competitionId)->where('club_id', $clubId)->paginate($per_page));
+                }
             }
             if(Auth::user()->user_type != 0 && $request->has('clubId')) {   
                 $clubId = $request->clubId;
