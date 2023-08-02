368cf1fc95fee556335160fac177e028fe48ffd4
Zeroooo0 <markocrncevic.92@gmail.com>
2023-08-02 11:06:08 +0200

    fix sort of registrations

--- app/Http/Controllers/V1/RegistrationsController.php
+++ app/Http/Controllers/V1/RegistrationsController.php
@@ -44,7 +44,11 @@ class RegistrationsController extends Controller
             }
             if(Auth::user()->user_type != 0 && $request->has('clubId')) {   
                 $clubId = $request->clubId;
-                return RegistrationsResource::collection(Registration::orderBy($sort, $sortDirection)->where('compatition_id', $competitionId)->where('club_id', $clubId)->paginate($per_page));
+                if($competition->is_abroad != 1) {
+                    return RegistrationsResource::collection(Registration::orderBy($sort, $sortDirection)->where('compatition_id', $competitionId)->where('club_id', $clubId)->paginate($per_page));
+                } else {
+                    return RegistrationsResource::collection(Registration::orderBy('id', 'desc')->where('compatition_id', $competitionId)->where('club_id', $clubId)->paginate($per_page));
+                }
             }
             if(Auth::user()->user_type == 0 && Auth::user()->club == null){
                 return $this->error('', 'Molimo vas da prvo kreirate klub!',403);
